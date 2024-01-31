3b193ab6e79e8befc25a9cfda9c7c1c25f1b536b
Nishchay Sinha <nishchay.space@gmail.com>
2024-01-31 18:40:50 +0530

    feat: fixed dates according to new db names

--- routes/videos.py
+++ routes/videos.py
@@ -2,10 +2,9 @@ from app import app, collection
 from flask import request
 from bson.json_util import dumps
 
-
 @app.route('/videos', methods=['GET'])
 def get_videos():
     page = request.args.get('page', 1, type=int)
     per_page = request.args.get('per_page', 10, type=int)
-    videos = collection.find().sort("published_at", -1).skip((page - 1) * per_page).limit(per_page)
+    videos = collection.find().sort("publishedAt", -1).skip((page - 1) * per_page).limit(per_page)
     return dumps(videos)  # Convert BSON to JSON
