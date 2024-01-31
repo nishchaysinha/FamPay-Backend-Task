cf0f0bb863ec28a182137636807efff1380cbab7
Nishchay Sinha <nishchay.space@gmail.com>
2024-01-31 12:39:40 +0530

    feat: updated config

--- utils/config.py
+++ utils/config.py
@@ -7,5 +7,7 @@ COLLECTION_NAME = os.getenv("COLLECTION_NAME")  # Load MongoDB collection name f
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 GOOGLE_API_KEYS = os.getenv("GOOGLE_API_KEYS").split(',')
+QUERY = os.getenv("QUERY")  # Load search query from .env file
+TIME_DELTA = int(os.getenv("TIME_DELTA"))  # Load time delta from .env file
 
 # Path: utils/config.py
